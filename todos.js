0afc8af30acdf86758d1c7d0423c353b3bae3f5e
Jim Falzone <jimfalzone@hotmail.com>
2022-03-22 17:55:41 -0400

    set up action framework in todos.js

--- todos.js
+++ todos.js
@@ -1,1 +1,22 @@
+todo = [];
 let action = prompt("What would you like to do?");
+while (action != 'quit') {
+
+    switch (action) {
+        case 'new':
+            console.log("you chose new");
+            break;
+        case 'list':
+            console.log("you chose list");
+            break;
+        case 'delete':
+            console.log("you chose delete")
+            break;
+        default: 
+            console.log('sorry, that is not a valid choice, please try again');
+    }
+    action = prompt("What would you like to do?");
+    
+}
+
+console.log("you just quit.  goodbye.")
