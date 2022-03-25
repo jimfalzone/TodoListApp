ec1e965f7c3bf68ee3ecf8b013e88a5bae65d976
Jim Falzone <jimfalzone@hotmail.com>
2022-03-25 17:31:19 -0400

    updates from video

--- todos.js
+++ todos.js
@@ -3,32 +3,33 @@ let action = prompt("What would you like to do?");
 let todolist = [];
 let deleteindex = '';
 
-while (action != 'quit') {
+while (action !== 'quit' && action !== 'q') {
 
     switch (action) {
         case 'new':
             console.log("you chose new");
             todo = prompt("Enter new todo")
-            todolist.push(todo)
-            console.log(todo);
-            console.log(todolist);
+            todolist.push(todo);
+            console.log(`${todo} added to the list!`);
             break;
         case 'list':
             // console.log("you chose list");
             // console.log(todo);
             console.log("**********")
-            for (let i = 0; i <= (todo.length - 2); i++) {
-                console.log((i+1) + ": " + todolist[i])
+            for (let i = 0; i < todo.length; i++) {
+                console.log(`${i}: ${todolist[i]}`)
             }
             console.log("**********")
             break;
         case 'delete':
             console.log("you chose delete")
             deleteindex = prompt("Enter the index of the item you want to delete.")
-            todolist.splice((deleteindex-1), 1);
+            parseInt(deleteindex);
+            const deleted = todolist.splice(deleteindex, 1);
+            console.log(`OK, deleted ${deleted[0]}`);
             break;
         default: 
-            console.log('sorry, that is not a valid choice, please try again');
+            console.log('sorry, that is not a vquitalid choice, please try again');
     }
     action = prompt("What would you like to do?");
     
